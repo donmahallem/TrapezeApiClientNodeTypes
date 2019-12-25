7892990ed85fbc6752e5743cb5015870fcfc0548
donmahallem <donmahallem@users.noreply.github.com>
2019-12-25 17:00:46 +0100

    fix deps

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 /*!
+ * Source https://github.com/donmahallem/TrapezeApiClientNodeTypes
+ */
+
+/*!
  * Source https://github.com/donmahallem/TrapezeApiClientNode
  */
+import { IVehicleLocation } from "@donmahallem/trapeze-api-types";
+
+export type IVehicleLocationExtended = IVehicleLocation | {
+    lastUpdate: number;
+};
