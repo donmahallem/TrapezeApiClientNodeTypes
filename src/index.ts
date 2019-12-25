55b5eff7c2dc18ff9f9812b5633fd476418c2d22
donmahallem <donmahallem@users.noreply.github.com>
2019-12-25 17:02:42 +0100

    fix tests

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@
  * Source https://github.com/donmahallem/TrapezeApiClientNodeTypes
  */
 
-/*!
- * Source https://github.com/donmahallem/TrapezeApiClientNode
- */
 import { IVehicleLocation } from "@donmahallem/trapeze-api-types";
 
 export type IVehicleLocationExtended = IVehicleLocation | {
