37f8bd6463eb6425c1d16f852061f33e4ac5e284
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-26 07:44:39 +0000

    Deploy donmahallem/TrapezeApiClientNodeTypes to github.com/donmahallem/TrapezeApiClientNodeTypes.git:gh-pages

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"4194304":"Type alias"},"rows":[{"id":0,"kind":4194304,"name":"IVehicleLocationExtended","url":"globals.html#ivehiclelocationextended","classes":"tsd-kind-type-alias"}]};
